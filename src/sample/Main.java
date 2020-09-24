de2bf7a74d8165f6ebf4fd7040be72e8a9f14fe4
Nikita Makarchuk <vfrfrfcbys@yandex.by>
2020-09-24 16:20:52 +0300

    add buttons

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,6 +5,7 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -14,21 +15,45 @@ import javafx.stage.Stage;
 
 
 public class Main extends Application {
-
+    private TableView table = new TableView();
     @Override
     public void start(Stage primaryStage) throws Exception {
         //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-
-        HBox mainHBox = new HBox();
-        TableView table = new TableView();
         Controller controller = new Controller();
-        table = controller.getTableDeliveryHistory();
-        VBox mainVBox = new VBox(mainHBox, table);
+
+        Button switchToTableAllPublication = new Button("AllPublication");
+        Button switchToCurrentSubscriptions = new Button("CurrentSubscriptions");
+        Button switchToDeliveryHistory = new Button("DeliveryHistory");
+
+
+        HBox mainHBox = new HBox(switchToTableAllPublication,
+                switchToCurrentSubscriptions, switchToDeliveryHistory);
+
+        VBox tableBox = new VBox(table);
+        VBox mainVBox = new VBox(mainHBox, tableBox);
 
 
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(mainVBox, 600, 500));
         primaryStage.show();
+
+        switchToTableAllPublication.setOnAction(e -> {
+            tableBox.getChildren().clear();
+            table = controller.getTableAllPublication();
+            tableBox.getChildren().add(table);
+
+        });
+        switchToCurrentSubscriptions.setOnAction(e -> {
+            tableBox.getChildren().clear();
+            table = controller.getTableCurrentSubscriptions();
+            tableBox.getChildren().add(table);
+        });
+        switchToDeliveryHistory.setOnAction(e -> {
+            tableBox.getChildren().clear();
+            table = controller.getTableDeliveryHistory();
+            tableBox.getChildren().add(table);
+        });
+
     }
 
     public static void main(String[] args) {
